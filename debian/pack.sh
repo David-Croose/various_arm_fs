551e7e15c1e7bd6302006e75a9a3815973f4bb40
David-Croose <178449962@qq.com>
2021-03-27 22:09:19 +0800

    debian packing script had been tested

--- debian/pack.sh
+++ debian/pack.sh
@@ -3,7 +3,7 @@
 set -e
 
 TMPMNT=mnt
-APTSRC=https://mirrors.tuna.tsinghua.edu.cn/debian/
+APTSRC=http://ftp2.cn.debian.org/debian
 
 if [ $(whoami) != root ]; then
 	echo 'error, you must be root'
@@ -26,19 +26,19 @@ read DEBVER
 
 case $DEBVER in
 	1)	DEBALIAS=buster
-		FSNAME=debian_"$DEBALIAS"_source
+		FSNAME=debian_10_"$DEBALIAS"_source
 		HTTPSRC=http://mirrors.ustc.edu.cn/debian/
 		;;
 	2)	DEBALIAS=stretch
-		FSNAME=debian_"$DEBALIAS"_source
+		FSNAME=debian_9_"$DEBALIAS"_source
 		HTTPSRC=http://mirrors.ustc.edu.cn/debian/
 		;;
 	3)	DEBALIAS=jessie
-		FSNAME=debian_"$DEBALIAS"_source
+		FSNAME=debian_8_"$DEBALIAS"_source
 		HTTPSRC=http://mirrors.ustc.edu.cn/debian/
 		;;
 	4)	DEBALIAS=jessie
-		FSNAME=debian_"$DEBALIAS"_linaro_source
+		FSNAME=debian_8_"$DEBALIAS"_linaro_source
 		HTTPSRC=https://releases.linaro.org/debian/images/alip-armhf/17.02/linaro-jessie-alip-20161117-32.tar.gz
 		;;
 	*)	echo 'error, you typed a wrong number'
@@ -64,45 +64,56 @@ if [ -n "$BLKDEV" ]; then
 fi
 set -e
 
-echo '================================================================================'
-echo 'processing the linaro debian...'
 if [ $DEBVER -eq 4 ]; then
-	wget $HTTPSRC $FSPATH/
-
+	echo '================================================================================'
+	echo "sorry, this function is still being developing"
+	#echo 'processing the linaro debian...'
+	#wget $HTTPSRC $FSPATH/
 	exit 0
 fi
 
 echo '================================================================================'
-echo 'processing the official debian...'
-apt-get install binfmt-support qemu qemu-user-static debootstrap
+echo 'installing essential packages...'
+apt-get install binfmt-support qemu qemu-user-static debootstrap multistrap
+
+echo '================================================================================'
+echo 'retrieving the official debian...'
 # ignore this log:
 # W: Cannot check Release signature; keyring file not available /usr/share/keyrings/debian-archive-keyring.gpg
 debootstrap --arch=armel --foreign $DEBALIAS $FSPATH $HTTPSRC
-cp /usr/bin/qemu-arm-static $FSPATH/usr/bin
-DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot $FSPATH debootstrap/debootstrap --second-stage
-#debootstrap --second-stage
 
+echo '================================================================================'
+echo 'debootstrap the official debian, this could take a big while...'
+cp /usr/bin/qemu-arm-static $FSPATH/usr/bin
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot $FSPATH debootstrap/debootstrap --second-stage --verbose
+
+echo '================================================================================'
+echo 'creating essential files for official debian...'
 echo "proc /proc proc defaults 0 0" >> $FSPATH/etc/fstab
 mkdir -p $FSPATH/usr/share/man/man1/
 mknod $FSPATH/dev/console c 5 1
-
-cp $FSPATH/etc/apt/sources.list $FSPATH/etc/apt/sources.list.bak
-echo "deb $APTSRC buster main" > $FSPATH/etc/apt/sources.list
-
 cat > $FSPATH/init_fs.sh << EOF
 #!bin/sh
+echo 'enter new root password:'
 passwd root
+
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "deb $APTSRC buster main" > /etc/apt/sources.list
 apt-get update
-apt-get install wpasupplicant udhcpc net-tools
-echo 'these packages is recommanded:'
-echo '    openssh-server git build-essential samba samba-common-bin nfs-kernel-server'
-echo '    xinetd tftp tftpd xfce4 dos2unix vim ctags unzip p7zip unrar'
+apt-get install wpasupplicant udhcpc net-tools openssh-server
+
 echo 'now you should install softwares you need by apt-get then exit'
+echo 'these packages is recommended:'
+echo '    git build-essential samba samba-common-bin nfs-kernel-server'
+echo '    xinetd tftp tftpd xfce4 dos2unix vim ctags unzip p7zip unrar e2fsprogs dosfstools'
 EOF
 chmod +x $FSPATH/init_fs.sh
 
+echo '================================================================================'
 echo 'now typing ./init_fs.sh'
 chroot $FSPATH
+
+rm -f $FSPATH/init_fs.sh
 
 if [ -n "$BLKDEV" ]; then
 	umount $BLKDEV
